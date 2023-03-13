0df9e66481de8e14991806f04c955acce8e0d754
pick8chu <po00076@gmail.com>
2023-03-13 11:53:59 +0100

    Revert "prettier + location"
    This reverts commit 4584918b44ea7b5cb392f5d8286ad643b574ebc2.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,21 @@
-jQuery(document).ready(function ($) {
-  $(".level-bar-inner").css("width", "0");
+jQuery(document).ready(function($) {
 
-  $(window).on("load", function () {
-    $(".level-bar-inner").each(function () {
-      var itemWidth = $(this).data("level");
+    $('.level-bar-inner').css('width', '0');
+    
+    $(window).on('load', function() {
 
-      $(this).animate(
-        {
-          width: itemWidth,
-        },
-        800
-      );
+        $('.level-bar-inner').each(function() {
+        
+            var itemWidth = $(this).data('level');
+            
+            $(this).animate({
+                width: itemWidth
+            }, 800);
+            
+        });
+
     });
-  });
+   
+    
+
 });
